199a177dfe5be16f817f17d5bf94afd0826e146e
Abhishek Das <dabhishek31oct@gmail.com>
2019-11-08 19:57:09 +0530

    Database Connection

--- todoApi/app.js
+++ todoApi/app.js
@@ -1,1 +1,29 @@
-console.log("API");
+const express = require('express');
+const app = express();
+const port = 5000;
+const db = require('./dbConnection');
+
+// connect to database
+db.connect((err) => {
+  if (err) {
+      throw err;
+  }
+  console.log('Connected to database');
+});
+global.db = db;
+
+
+
+app.get('/',(req,res) => {
+  // let query = "SELECT * FROM `todoBuckets` ORDER BY id ASC"; // query database to get all the players
+  // db.query(query, (err, result) => {
+  //     if (err) {
+  //         res.redirect('/');
+  //     }
+    res.json('result')
+  // });
+})
+
+app.listen(port,() => {
+  console.log(`Server is live : http://localhost:${port}`);
+});
