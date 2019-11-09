042881a776bdb6b04459da3c79233c17be4b87ab
Abhishek Das <dabhishek31oct@gmail.com>
2019-11-09 16:08:45 +0530

    Adding Assets

--- todo/src/components/sidebar.js
+++ todo/src/components/sidebar.js
@@ -4,11 +4,15 @@ import { Link } from 'react-router-dom';
 const sidebar = () => {
 	return (
 		<div className="sidebarContents">
-			<Link exact to="/">
-				To Do Application
-			</Link>
-			<Link to="/create/note">Create Note</Link>
-			<Link to="/create/bucket">Create Bucket</Link>
+			<div className="app-name">
+				<Link exact to="/">
+					To Do Application
+				</Link>
+			</div>
+			<div className="sub-menu">
+				<Link to="/create/note">Create Note</Link>
+				<Link to="/create/bucket">Create Bucket</Link>
+			</div>
 		</div>
 	);
 };
